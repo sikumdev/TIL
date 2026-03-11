2c9888bee5e0a52747df00ee8c4b60354af28cef
sikumdev <sikumdev@gmail.com>
2026-03-11 21:22:00 +0900

    internet_search 툴 추가

--- 05-deepagent/tools.py
+++ 05-deepagent/tools.py
@@ -1,8 +1,11 @@
 # tools.py
 
 import os
+from typing import Literal
 from slack_sdk import WebClient
+from tavily import TavilyClient
 from langchain.tools import tool
+
 
 from backend import dt_backend
 
@@ -32,3 +35,22 @@ def send_slack_message(text: str, file_path: str | None = None) -> str:
         )
 
     return 'Message sent.'
+
+
+TAVILY_API_KEY = os.getenv('TAVILY_API_KEY')
+tavily_client = TavilyClient(api_key=TAVILY_API_KEY)
+
+@tool
+def internet_search(
+    query: str,
+    max_results: int = 5,  # = 뒤에는 기본값
+    topic: Literal['general', 'news', 'finance'] = 'general',
+    include_raw_content: bool = False,
+):
+    """Run a web search"""
+    return tavily_client.search(
+        query,
+        max_results=max_results,
+        include_raw_content=include_raw_content,
+        topic=topic
+    )
